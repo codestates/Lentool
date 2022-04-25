de1427e17f4057aa97e80cdb159f73320a7891b0
hohokie <nhpunch@gmail.com>
2022-04-25 15:19:43 +0900

    [Create] client

---
+++ client/src/App.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import './App.css';
+import { useGetPokemonByNameQuery } from './services/pokemon';
+
+function App() {
+  const { data, error, isLoading } = useGetPokemonByNameQuery('bulbasaur')
+
+  return (
+    <div className="App">
+      {error ? (
+        <>Oh no, there was an error</>
+      ) : isLoading ? (
+        <>Loading...</>
+      ) : data ? (
+        <>
+          <h3>{data.species.name}</h3>
+          <img src={data.sprites.front_shiny} alt={data.species.name} />
+        </>
+      ) : null}
+    </div>
+  );
+}
+
+export default App;
