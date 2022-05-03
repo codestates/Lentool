9d146306edf72e1b4b76ef9494aa0d0f212ad648
JKS5 <jokekhanson@gmail.com>
2022-05-03 22:30:08 +0900

    app/store.ts작은 병합 오류 수정

--- client/src/app/reducers.ts
+++ client/src/app/reducers.ts
@@ -5,12 +5,9 @@ import login from "feature/login/loginSlice";
 import auth from "../feature/login/authSlice";
 import myinfo from "../feature/mypage/myinfoSlice";
 import posts from "../feature/post/postSlice";
-<<<<<<< HEAD
-=======
 import detailPost from "feature/post/detailPostSlice";
 import trial from "../feature/home/trialSlice";
 import rooms from "../feature/chat/roomSlice";
->>>>>>> 3ab539747f8780e5000a4e2294c24fa282a96740
 
 const persistConfig = {
   key: "root",
@@ -21,12 +18,9 @@ const rootReducer = combineReducers({
   login,
   myinfo,
   posts,
-<<<<<<< HEAD
-=======
   detailPost,
   trial,
   rooms,
->>>>>>> 3ab539747f8780e5000a4e2294c24fa282a96740
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
