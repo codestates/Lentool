596d972b3a725bb14d5dfb86153e7211c6bf97b5
kaisiok <okop10@nate.com>
2022-05-06 11:13:24 +0900

    회원탈퇴 오류수정, 포스트 삭제 추가

--- server/routes/tools.js
+++ server/routes/tools.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
-const { tools } = require("../controllers/tools");
+const { tools, deletePost } = require("../controllers/tools");
 const multer = require("multer");
 const upload = multer({ dest: "./postimg" });
 
 router.post("/", upload.array("photo"), tools);
+router.delete("/:id", deletePost);
 
 module.exports = router;
